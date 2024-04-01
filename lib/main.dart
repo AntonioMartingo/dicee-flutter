af87f5445031f05567c56b534bf8b48409341ef5
AntonioMartingo <106592165+AntonioMartingo@users.noreply.github.com>
2024-04-01 22:04:14 +0100

    Acabado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,70 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      print('Left button got pressed.');
+                      setState(() {
+                        leftDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      print('Right button got pressed.');
+                      setState(() {
+                        rightDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: rollDice,
+            child: Text(
+              'Randomizar',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
